86ae0ccc5b9296ac19fbe07a4544cfb5a7b09bbf
Oleg595 <60892746+Oleg595@users.noreply.github.com>
2021-03-15 12:15:21 +0300

    Initial commit

---
+++ OneDimensionalMinimization/Method.cpp
@@ -0,0 +1,18 @@
+#include "IMethod.h"
+#include "ParabolicMethod.h"
+#include "DichotomyMethod.h"
+
+IMethod* IMethod::create_method(METHOD method, IFunction* const function, double tol, std::pair<double, double> const& section) {
+	if (section.first > section.second) {
+		return nullptr;
+	}
+	switch (method) {
+	case METHOD::PARABOLIC:
+		return new ParabolicMethod(function, tol, section);
+	case METHOD::DICHOTOMY:
+		return new DichotomyMethod(function, tol, section);
+	}
+	return nullptr;
+}
+
+IMethod::~IMethod() {}
